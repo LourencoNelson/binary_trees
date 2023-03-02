1286ce86f79578208994bae94554a59177897fa9
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-03-02 16:38:26 +0200

    binary tree tasks

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,9 +9,9 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = (binary_tree_t *) malloc(sizeof(binary_tree_t));
-	
+
 	if (node == NULL)
-		return NULL;
+		return (NULL);
 
 	node->parent = parent;
 	node->left = NULL;
