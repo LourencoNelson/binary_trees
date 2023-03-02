fc05a57c4980b1d7bc91201bc6cbf8cfc6f32cba
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-03-02 18:01:27 +0200

    binary tree tasks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -23,9 +23,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	{
 		node->left = parent->left;
 		parent->left->parent = node;
+		parent->left = node;
 	}
-
-	parent->left = node;
+	else
+	{
+		parent->left = node;
+	}
 
 	return (node);
 }
